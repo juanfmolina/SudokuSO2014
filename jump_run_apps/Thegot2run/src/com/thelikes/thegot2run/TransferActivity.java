f54fd7208687d5df913e6500380f56c8c1fca7df
Juan Fernando Molina Florez <jufermoflo134@hotmail.com>
2014-03-10 19:28:03 -0500

    Cambios para verificar la conectividad

--- jump_run_apps/Thegot2run/src/com/thelikes/thegot2run/TransferActivity.java
+++ jump_run_apps/Thegot2run/src/com/thelikes/thegot2run/TransferActivity.java
@@ -1,11 +1,16 @@
 package com.thelikes.thegot2run;
 
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class TransferActivity extends Activity {
@@ -17,7 +22,25 @@ public class TransferActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Parse.initialize(this, "YqFw4gRsRwBlvL9UeN2hPkTZ0V1MHid1ptSVJbHY", "L85qVmSj5aQAQeyVKXI4CRHMUfPbhPcJe1x1v1pu");
 		setContentView(R.layout.activity_transfer);
+		Button aceptar = (Button) findViewById(R.id.botonAgregar);
+		final Intent intent =  new Intent(this, ScoresActivity.class);
+		aceptar.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Puntuacion puntaje = new Puntuacion(campoNombre.getText().toString(),scoreJuego,fecha);				
+				ParseObject ObjetoParse = new ParseObject("Score");
+				ObjetoParse.put("Name", campoNombre.getText().toString());
+				ObjetoParse.put("score", scoreJuego);
+				ObjetoParse.saveInBackground();
+				intent.putExtra("nuevoPuntaje", puntaje);
+				startActivity(intent);
+				
+				
+			}
+		});
 	}
 
 	@Override
@@ -43,10 +66,6 @@ public class TransferActivity extends Activity {
 	
 
 		public void onClick(View view){
-			Puntuacion puntaje = new Puntuacion(campoNombre.getText().toString(),scoreJuego,fecha);
-			Intent intent =  new Intent(this, ScoresActivity.class);
-			intent.putExtra("nuevoPuntaje", puntaje);
-			startActivity(intent);
 
 		}
 	
